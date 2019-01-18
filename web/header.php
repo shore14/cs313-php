f568418c88b70e1834f08d4371bd20185ad432b1
Andrew Shore <andrewshore1414@gmail.com>
2019-01-17 20:00:46 -0700

    css header

--- web/header.php
+++ web/header.php
@@ -1,5 +1,9 @@
+<link rel="stylesheet" type="text/css"  href="home.css" />
+
 <div>
-<h2>MAke this work</h2>
-<a href="home.php">Home is Home</a>
-<a href="page2.php">Construction Page</a>
+<h2>Links to things</h2>
+<div class="nav">
+    <a href="home.php">Home is Home</a>
+    <a href="page2.php">Construction Page</a>
+</div>
 </div>

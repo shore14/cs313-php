42332b5fd659822f0c7cd9d038db897de674a11c
Andrew Shore <andrewshore1414@gmail.com>
2019-01-22 10:38:50 -0700

    name

--- web/t03/results.php
+++ web/t03/results.php
@@ -1,3 +1,14 @@
-<?php
-
-?>
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>j</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+   Your name is: <?php  echo $_GET["name"] . " " . $_GET["last"] ?> 
+</body>
+</html>

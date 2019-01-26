2bb891ee5943115b325c3358242564e93ce40837
Andrew Shore <andrewshore1414@gmail.com>
2019-01-26 16:29:24 -0700

    var

--- web/func.php
+++ web/func.php
@@ -5,22 +5,22 @@
     $car = $_REQUEST["c"];
 
       if($car == 'lotus'){
-          $_SESSION['lotus'] = 'lotus'
+          $_SESSION['lotus'] = 'lotus';
       }
       if($car == 'chaser'){
-          $_SESSION['chaser'] = 'chaser'
+          $_SESSION['chaser'] = 'chaser';
       }
       if($car == 'lambo'){
-          $_SESSION['lambo'] = 'lambo'
+          $_SESSION['lambo'] = 'lambo';
       }
       if($car == 'ferrari'){
-          $_SESSION['ferrari'] = 'ferrari'
+          $_SESSION['ferrari'] = 'ferrari';
       }
       if($car == 'mazz'){
-          $_SESSION['mazz'] = 'mazz'
+          $_SESSION['mazz'] = 'mazz';
       }
       if($car == 'gtr'){
-          $_SESSION['gtr'] = 'gtr'
+          $_SESSION['gtr'] = 'gtr';
       }
 //  print_r($_SESSION);
 ?>

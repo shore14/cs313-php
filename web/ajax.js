e961aca0b9daf771f717ed83a5e2632c1cca724a
Andrew Shore <andrewshore1414@gmail.com>
2019-01-26 16:21:31 -0700

    var

--- web/ajax.js
+++ web/ajax.js
@@ -1,9 +1,5 @@
-<html>
-<script type="text/javascript">
     function sendCar(str) {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.open("GET", "func.php?c=" + str, true);
         xmlhttp.send();
     }
-</script>
-</html>

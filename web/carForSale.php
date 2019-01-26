e58f5a76b1a738805c027e079647f525d79d021d
Andrew Shore <andrewshore1414@gmail.com>
2019-01-26 15:17:46 -0700

    var

--- web/carForSale.php
+++ web/carForSale.php
@@ -10,6 +10,7 @@ session_start();
     <title>Page Title</title> 
     <meta name="viewport" content="width=device-width, initial-scale=1"> 
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="ajax.js"></script>
    </head>
 <body>
  <h1>Get Your Next Midlife Crisis Here!</h1>

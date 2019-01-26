6f30c2d0cac8a09d6e9f980722c2c55e30d0eedf
Andrew Shore <andrewshore1414@gmail.com>
2019-01-26 11:48:35 -0700

    var

--- web/collect.php
+++ web/collect.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
+header("carForSale.php");
 $car = $_POST['car'];
 
  $_SESSION["car"] = $car; 
  print_r($_SESSION);
-//  header("carForSale.php");
-window.location.href = "carForSale.php";
+// 
+
 
 ?>

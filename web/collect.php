030b67a4efcccc130c055c5941d25ae08e745a05
Andrew Shore <andrewshore1414@gmail.com>
2019-01-26 11:18:14 -0700

    var

--- web/collect.php
+++ web/collect.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
+$chaser = $_POST['lotus'];
+
  echo $_SESSION["lotus"] = $_POST['lotus'];
- echo $_SESSION["chaser"] = $_POST['chaser'];
+ echo $_SESSION["chaser"] = $chaser;
  print_r($_SESSION);
 ?>

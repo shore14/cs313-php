8a3d21580d471231daab1c011c93f79946dac65c
Andrew Shore <andrewshore1414@gmail.com>
2019-02-07 10:40:29 -0700

    query

--- web/t03/home.php
+++ web/t03/home.php
@@ -17,7 +17,7 @@ try {
     $dbPassword = $dbOpts["pass"];
     $dbName - ltrim($dbOpts["path"], '/');
 
-    $db = new PDO("pgsql:host=$dbHost,port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) {

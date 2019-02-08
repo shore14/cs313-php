cb2de8cd4d6fc4ad8220142284d00339377ac472
Andrew Shore <andrewshore1414@gmail.com>
2019-02-07 19:12:00 -0700

    login

--- web/chlogin.php
+++ web/chlogin.php
@@ -5,4 +5,19 @@
   $first = $_GET['first'];
   $pass = $_GET['pass'];
   $iden = $_GET['iden'];
+
+  $statment = $db->prepare('SELECT first, pass FROM people WHERE first=:first AND pass=:pass AND iden=:iden');
+  $statment = bindValue(':first', $first, PDO::PARAM_STR);
+  $statment = bindValue(':pass', $pass, PDO::PARAM_STR);
+  $statment = bindValue(':iden', $iden, PDO::PARAM_STR);
+  $statment->execute();
+  $rows = $statment->fetchAll(PDO::FETCH_ASSOC);
+
+   if($rows != null) {
+       echo "Thanks for login in";
+   }
+   else {
+       echo "Wrong info";
+   }
+
 ?>

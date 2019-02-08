f76985d06a5a3518a60ba33dc8b0dc37c34730d9
Andrew Shore <andrewshore1414@gmail.com>
2019-02-08 09:47:06 -0700

    login

--- web/login.php
+++ web/login.php
@@ -11,7 +11,6 @@
     <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css"  href="login.css" />
-    <script src="main.js"></script>
 </head>
 <body>
 <?php include 'header.php'?>

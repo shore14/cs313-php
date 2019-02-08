7ac10bc4ebc0dc530adea44ccc46003d36071c3e
Andrew Shore <andrewshore1414@gmail.com>
2019-02-08 10:15:43 -0700

    login

--- web/chlogin.php
+++ web/chlogin.php
@@ -6,7 +6,7 @@
   $passw = $_GET['passw'];
   $iden = $_GET['iden'];
 
-  $statment = $db->prepare('SELECT * FROM people WHERE first=:first AND passw=:passw');
+  $statment = $db->prepare("SELECT * FROM people WHERE first=:first AND passw=:passw");
   $statment = bindValue(':first', $first, PDO::PARAM_STR);
   $statment = bindValue(':passw', $passw, PDO::PARAM_STR);
   $statment = bindValue(':iden', $iden, PDO::PARAM_STR);

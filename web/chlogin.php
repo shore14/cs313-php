458a8f84b352ac4fa63cc894afd716cb4fd08ab4
Andrew Shore <andrewshore1414@gmail.com>
2019-02-09 09:39:26 -0700

    login

--- web/chlogin.php
+++ web/chlogin.php
@@ -16,7 +16,7 @@
   $statment = $db->prepare('SELECT firstname FROM people WHERE firstname = :firstname');
  // $statment = bindValue(':first', $first, PDO::PARAM_STR);
  // $statment = bindValue(':passw', $passw, PDO::PARAM_STR);
-  $statment->bindValue(':firstname', $firstname, PDO::PARAM_STR);
+  $statment = bindValue(':firstname', $firstname, PDO::PARAM_STR);
   $statment->execute();
   $rows = $statment->fetchAll(PDO::FETCH_ASSOC);
 

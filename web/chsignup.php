7f97994baf41df1e1b01ff6268d2c6e6c3188561
Andrew Shore <andrewshore1414@gmail.com>
2019-02-09 10:29:16 -0700

    signup

---
+++ web/chsignup.php
@@ -0,0 +1,32 @@
+<?php
+  require 'db.php';
+  $db = getdb();
+
+  $firstname = $_GET['first'];
+  $lastname = $_GET['last'];
+  $passw = $_GET['passw'];
+  $iden = $_GET['iden'];
+ 
+
+  $statment = $db->prepare('INSERT INTO people (firstname, lastname, pass, iden) 
+                            VALUES(:firstname, :lastname, :passw, ;iden)');
+  $statment->bindValue(':firstname', $firstname, PDO::PARAM_STR);
+  $statment->bindValue(':lastname', $lastname, PDO::PARAM_STR);
+  $statment->bindValue(':passw', $passw, PDO::PARAM_STR);
+  $statment->bindValue(':iden', $iden, PDO::PARAM_STR);
+  $statment->execute();
+  //$rows = $statment->fetchAll(PDO::FETCH_ASSOC);
+
+  foreach($db->query('select * from scriptures') as $row) {
+     echo $row['id'] . " " . $row['firstname'] . " " . $row['pass'] . " " . $row['iden'] . '<br><br>';
+ }
+  
+
+    // if($statment->rowCount() > 0) {
+    //     echo "Thanks for login in";
+    // }
+    // else {
+    //     echo "Wrong info";
+    // }
+
+?>
--- web/signup.php
+++ web/signup.php
@@ -1,5 +1,23 @@
 <?php
 require 'db.php';
 $db = getdb();
+<?php include 'header.php'?>
 
+<form action="chsignup.php" method="GET">
+    <div class="boxlogin">
+        <label ><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="first" value="<?php echo $first;?>" required>
+
+        <label ><b>LastName</b></label>
+        <input type="text" placeholder="Enter LastName" name="first" value="<?php echo $last;?>" required
+
+        <label ><b>Password</b></label>
+        <input type="text" placeholder="Enter Password" name="passw" value="<?php echo $passw;?>"required>
+
+        <label ><b>Identifier</b></label>
+        <input type="text" placeholder="Enter Your unique" name="iden" value="<?php echo $iden;?>"required>
+
+        <button type="submit">Login</button>
+    </div>
+</form>
 ?>

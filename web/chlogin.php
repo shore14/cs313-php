bb268d5ab39e90ddd78b26e37be5ca1843c8c509
Andrew Shore <andrewshore1414@gmail.com>
2019-02-09 09:38:11 -0700

    login

--- web/chlogin.php
+++ web/chlogin.php
@@ -13,7 +13,7 @@
          echo $row['firstname'] . " " . $row['pass'] . ":" . $row['iden'] . '<br>';
      }
 
-  $statment = $db->prepare('SELECT firstname, pass, iden FROM people WHERE firstname = :firstname');
+  $statment = $db->prepare('SELECT firstname FROM people WHERE firstname = :firstname');
  // $statment = bindValue(':first', $first, PDO::PARAM_STR);
  // $statment = bindValue(':passw', $passw, PDO::PARAM_STR);
   $statment->bindValue(':firstname', $firstname, PDO::PARAM_STR);

e6b25fddecc14136788fbc5710971808e2234da0
Andrew Shore <andrewshore1414@gmail.com>
2019-02-09 09:10:49 -0700

    login

--- web/chlogin.php
+++ web/chlogin.php
@@ -9,12 +9,19 @@
   echo $passw;
   echo $iden;
 
-  $statment = $db->prepare("SELECT * FROM people WHERE first=:first AND passw=:passw");
+   foreach($db->query('select * from people') as $row) {
+         echo $row['firs'] . " " . $row['pass'] . ":" . $row['iden'] . '<br>';
+     }
+
+  $statment = $db->prepare("SELECT * FROM people WHERE first=:first AND pass=:passw");
   $statment = bindValue(':first', $first, PDO::PARAM_STR);
   $statment = bindValue(':passw', $passw, PDO::PARAM_STR);
   //$statment = bindValue(':iden', $iden, PDO::PARAM_STR);
   $statment->execute();
 //  $rows = $statment->fetchAll(PDO::FETCH_ASSOC);
+
+
+
   while ($rows = $statment->fetch(PDO::FETCH_ASSOC)){
       echo $rows['first'];
   }

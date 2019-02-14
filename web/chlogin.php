8eb8c3420954451dd80fa5851880b5bda8e36094
Andrew Shore <andrewshore1414@gmail.com>
2019-02-14 14:59:25 -0700

    signup

--- web/chlogin.php
+++ web/chlogin.php
@@ -5,28 +5,21 @@
   $firstname = $_GET['first'];
   $passw = $_GET['passw'];
   $iden = $_GET['iden'];
- // echo $first;
-  //echo $passw;
-  //echo $iden;
 
-  //  foreach($db->query('select * from people') as $row) {
-  //        echo $row['firstname'] . " " . $row['pass'] . ":" . $row['iden'] . '<br>';
-  //    }
 
-  $statment = $db->prepare('SELECT firstname FROM people WHERE firstname = :firstname');
- // $statment = bindValue(':first', $first, PDO::PARAM_STR);
- // $statment = bindValue(':passw', $passw, PDO::PARAM_STR);
+  $statment = $db->prepare('SELECT firstname, pass, iden FROM people 
+                            WHERE firstname = :firstname
+                            AND   pass = :passw
+                            AND   iden = :iden');
   $statment->bindValue(':firstname', $firstname, PDO::PARAM_STR);
+  $statment->bindValue(':passw', $passw, PDO::PARAM_STR);
+  $statment->bindValue(':iden', $iden, PDO::PARAM_STR);
   $statment->execute();
   $rows = $statment->fetch(PDO::FETCH_ASSOC);
 
-
   
-  // while ($rows = $statment->fetch(PDO::FETCH_ASSOC)){
-  //     echo $rows['firstname'];
-//  }
     if($statment->rowCount() > 0) {
-        echo "Thanks for login in";
+      header('Location:feed.php');
     }
     else {
         echo "Wrong info";

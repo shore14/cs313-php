29378f371dffc3d59b4b835144b9f858b9773542
Andrew Shore <andrewshore1414@gmail.com>
2019-02-24 11:21:01 -0700

    ids

--- web/create.php
+++ web/create.php
@@ -4,11 +4,12 @@
 
   $title = $_POST['title'];
   $body = $_POST['body'];
-
-  $state = $db->prepare('INSERT INTO cards (title, body) 
-                        VALUES (:title, :body)');
+  $type = $_POST['type'];
+  $state = $db->prepare('INSERT INTO cards (title, body, type) 
+                        VALUES (:title, :body, (SELECT id FROM types where type == :type))'); 
   $state->bindValue(':title', $title, PDO::PARAM_STR);
   $state->bindValue(':body', $body, PDO::PARAM_STR);
+  $state->bindValue(':type', $type, PDO::PARAM_STR);
   $state->execute();
 
   header('Location: feed.php');
--- web/feed.php
+++ web/feed.php
@@ -16,13 +16,18 @@
        <h1>Put nice things here!<h1> 
        <div class="items">
          <?php
-            $content = $db->query('SELECT title, body FROM cards ORDER BY id DESC LIMIT 10');
+            $content = $db->query('SELECT title, body,
+            (SELECT t.type FROM types t join cards c ON t.id = c.type_id) 
+            FROM cards ORDER BY id DESC LIMIT 10');
+            // $content2 = $db->query('SELECT c.title, c.body, t.type
+            //                         FROM cards c, types t
+            //                         JOIN types')
            // $content->execute();
           // $rows = $content->fetchAll(PDO::FETCH_ASSOC);
 
           foreach($content as $row)
           {
-            echo '<div class="cards">' . strtoupper($row['title']) . '<br>' . $row['body'] . '<br><br></div><br><br>';
+            echo '<div class="cards">'. $row['type_id'] . strtoupper($row['title']) . '<br>' . $row['body'] . '<br><br></div><br><br>';
           }
         ?>
     
@@ -31,6 +36,8 @@
        <form action="create.php" method="POST" class="insert-card">
            <input type="text" placeholder="title" name="title">
            <input type="text" placeholder="comment" name="body">
+           <input type="radio" name="type" value="goodDeed"> good deed <br>            
+            <input type="radio" name="type" value="quote"> quote <br>            
            <button type="submit">Add a nice thing</button>
        </form>
     </div>

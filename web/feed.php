251e8b8a50d6db76c4b7cf416a3fddae7a5324d6
Andrew Shore <andrewshore1414@gmail.com>
2019-02-23 21:16:00 -0700

    content

--- web/feed.php
+++ web/feed.php
@@ -11,9 +11,21 @@
     <link rel="stylesheet" type="text/css"  href="feed.css"/>
 </head>
 <body>
-    <div class="feedbox">
+
+       <div class="feedbox">
        <h1>Put nice things here!<h1> 
        <div class="items">
+    <?php
+      $content = $db->query('SELECT title, body FROM cards ORDER BY id DESC LIMIT 10');
+      $content->execute();
+      $rows = $content->fetchAll(PDO::FETCH_ASSOC);
+
+      foreach($content as $row)
+      {
+        echo $row['title'] . '<br>' . $row['body'] . '<br><br>';
+      }
+    ?>
+    
 
        </div>
        <form action="create.php" method="POST" class="insert-card">

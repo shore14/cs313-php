79c411df32365b08e0546b7c61f28b792ee3645c
Andrew Shore <andrewshore1414@gmail.com>
2019-02-24 13:10:48 -0700

    ids

--- web/feed.php
+++ web/feed.php
@@ -24,7 +24,7 @@
            // $content->execute();
           // $rows = $content->fetchAll(PDO::FETCH_ASSOC);
 
-          foreach($content as $row)
+          foreach($content2 as $row)
           {
             echo '<div class="cards">'. strtoupper($row['c.title']) . '<br>' . $row['c.body'] . '<br><br></div><br><br>';
           }

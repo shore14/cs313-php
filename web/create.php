76bbda0b5680496f13c621c181076a688c553a31
Andrew Shore <andrewshore1414@gmail.com>
2019-02-24 21:46:36 -0700

    ids

--- web/create.php
+++ web/create.php
@@ -8,7 +8,7 @@
   // $state = $db->prepare('INSERT INTO cards (title, body) 
   //                       VALUES (:title, :body)'); 
   $state = $db->prepare('INSERT INTO cards (title, body, type) 
-                         VALUES (:title, :body, ("SELECT id FROM types where type = :typ"))'); 
+                         VALUES (:title, :body, SELECT id FROM types where type = :typ)'); 
   $state->bindValue(':title', $title, PDO::PARAM_STR);
   $state->bindValue(':body', $body, PDO::PARAM_STR);
   $state->bindValue(':typ', $type, PDO::PARAM_STR);

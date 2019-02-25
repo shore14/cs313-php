dcb1f58eed2913b0211789b57dc66809dcaeed8c
Andrew Shore <andrewshore1414@gmail.com>
2019-02-24 22:12:34 -0700

    ids

--- web/feed.php
+++ web/feed.php
@@ -7,7 +7,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
     <title>See nice things</title>
     <link rel="stylesheet" type="text/css"  href="feed.css"/>
 </head>

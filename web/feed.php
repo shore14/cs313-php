9121320fca34ccd53cca9f2cbf5b7be9f5b02f6f
Andrew Shore <andrewshore1414@gmail.com>
2019-02-24 22:15:46 -0700

    ids

--- web/feed.php
+++ web/feed.php
@@ -7,11 +7,15 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <title>See nice things</title>
     <link rel="stylesheet" type="text/css"  href="feed.css"/>
 </head>
 <body>
-
+     <div class="container">
        <div class="feedbox">
        <h1>Put nice things here!</h1> 
        <div class="items">
@@ -46,5 +50,7 @@
            <button type="submit">Add a nice thing</button>
        </form>
     </div>
+          </div>
 </body>
+
 </html>

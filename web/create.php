b16be72d82ff29344093ba7d7705056174b641cb
Andrew Shore <andrewshore1414@gmail.com>
2019-02-24 21:58:07 -0700

    ids

--- web/create.php
+++ web/create.php
@@ -7,16 +7,16 @@
   $type = $_POST['type'];
   // $state = $db->prepare('INSERT INTO cards (title, body) 
   //                       VALUES (:title, :body)'); 
-  $typeid = $db->prepare('SELECT id FROM types where context = :typ');
-  $typeid->bindValue(':typ', $type, PDO::PARAM_STR);
-  $typeid->execute();
-  $id = $typeid->fetch(PDO::FETCH_ASSOC);
+  // $typeid = $db->prepare('SELECT id FROM types where context = :typ');
+  // $typeid->bindValue(':typ', $type, PDO::PARAM_STR);
+  // $typeid->execute();
+  // $id = $typeid->fetch(PDO::FETCH_ASSOC);
 
-  $state = $db->prepare('INSERT INTO cards (title, body) 
-                         VALUES (:title, :body, $id)'); 
+  $state = $db->prepare('INSERT INTO cards (title, body, type_id) 
+                         VALUES (:title, :body, (SELECT id FROM types WHERE context = :typ))'); 
   $state->bindValue(':title', $title, PDO::PARAM_STR);
   $state->bindValue(':body', $body, PDO::PARAM_STR);
-  
+  $state->bindValue(':typ', $type, PDO::PARAM_STR);
   $state->execute();
 
   header('Location: feed.php');
